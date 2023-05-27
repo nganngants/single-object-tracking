3527cd4a35619eb59185c159635d72c8693dc058
Elise <21520069@gm.uit.edu.vn>
2023-05-27 23:09:30 +0700

    update metric, add TLPAtrr dataset

--- single_object_tracking.py
+++ single_object_tracking.py
@@ -15,28 +15,41 @@ def compute_iou(ground_truth, prediction_box):
   iou = ops.box_iou(ground_truth_bbox, prediction_bbox)
   return iou.numpy()[0][0]
 
+def success_rate (ground_truths, predictions, threshold = 0.5):
+  total_frames = len(ground_truths)
+  successful_frames = 0
+  for i in range(total_frames):
+    if ground_truths[i] == None or predictions[i] == None:
+      successful_frames += (ground_truths == None and predictions[i] == None)
+      continue
+    iou = compute_iou(ground_truths[i], predictions[i])
+    if (iou >= threshold):
+       successful_frames += 1
+  
+  return successful_frames, successful_frames / total_frames
 
-def tracking (data_dir):
 
-  # List all the images in the directory
+def tracking (data_dir, threshold = 0.5):
+
   image_dir = os.path.join(data_dir, 'img')
   image_files = sorted(os.listdir(image_dir))
 
   # Read the first image
   frame = cv2.imread(os.path.join(image_dir, image_files[0]))
-
   # Parse the ground truth annotation for the first frame
   gt_file = os.path.join(data_dir, 'groundtruth_rect.txt')
   with open(gt_file, 'r') as f:
     gt_annotations = [tuple(map(int, line.strip().split(','))) for line in f]
   (_, gt_x, gt_y, gt_w, gt_h, is_lost) = gt_annotations[0]
+
   # Initialize the tracker
-  tracker = cv2.legacy.TrackerKCF_create()
+  tracker = cv2.TrackerKCF_create()
   tracker.init(frame, (gt_x, gt_y, gt_w, gt_h))
 
-  # Initialize variables for tracking evaluation
   num_frames = len(image_files)
   iou_sum = 0
+  ground_truths = []
+  predictions = []
 
   # Loop through each image in the sequence
   for i in range(1, num_frames):
@@ -51,50 +64,108 @@ def tracking (data_dir):
 
       # Compute the intersection over union (IoU) between the predicted and ground truth bounding boxes
       if success:
-          if is_lost:
-            continue
           x, y, w, h = [int(k) for k in bbox]
           pred_box = [x, y, x + w, y + h]
+          predictions.append(pred_box)
+          if is_lost:
+            ground_truths.append(None)
+            continue
           gt_box = [gt_x, gt_y, gt_x + gt_w, gt_y + gt_h]
-          iou = compute_iou(pred_box, gt_box)
-          iou_sum += iou
+          
+          ground_truths.append(gt_box)
       else:
-          iou_sum += is_lost
+        predictions.append(None)
+        if not is_lost:
+          gt_box = [gt_x, gt_y, gt_x + gt_w, gt_y + gt_h]
+          ground_truths.append(gt_box)
+        else:
+          ground_truths.append(None)
 
       # Draw bounding box and ground truth annotation
-      if success:
-          cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-      cv2.rectangle(frame, (gt_x, gt_y), (gt_x + gt_w, gt_y + gt_h), (0, 0, 255), 2)
+      # if success:
+      #     cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+      # cv2.rectangle(frame, (gt_x, gt_y), (gt_x + gt_w, gt_y + gt_h), (0, 0, 255), 2)
 
-      # Display the image
-      cv2.imshow('Frame', frame)
+      # #Display the image
+      # cv2.imshow('Frame', frame)
 
       # Exit if ESC pressed
       if cv2.waitKey(1) == 27:
           return -1, -1
 
-  # Compute the tracking evaluation metrics
-  avg_iou = iou_sum / (num_frames - 1)
 
   # Release resources
   cv2.destroyAllWindows()
+  succ_frames, succ_rate = success_rate(ground_truths, predictions, threshold)
+  return num_frames, succ_frames, succ_rate
 
-  return num_frames, avg_iou
+def runTLPAtrr():
 
-dir_datasets = './TinyTLP'
-all_data = os.listdir(dir_datasets)
-sum_frames = 0
-sum_iou = 0.0
-for data in all_data:
-   path = os.path.join(dir_datasets, data)
-   num, iou = tracking(path)
-   if iou == -1:
-      break
-   sum_iou += iou * num
-   sum_frames += num
-   print(data, iou)
+  attributes = {
+    'bc': 'background clutter',
+    'fm': 'fast motion',
+    'iv': 'illumination variation',
+    'occ': 'partial occlusions',
+    'ov': 'out of view',
+    'sv': 'large scale variation'
+    }
+  sum_succ = {
+    'bc': 0,
+    'fm': 0,
+    'iv': 0,
+    'occ': 0,
+    'ov': 0,
+    'sv': 0
+  }
+  sum_frames = {
+     'bc': 0,
+    'fm': 0,
+    'iv': 0,
+    'occ': 0,
+    'ov': 0,
+    'sv': 0
+  }
+  dir_datasets = './TLPAttr'
+  all_data = os.listdir(dir_datasets)
+  for data in all_data:
+    path = os.path.join(dir_datasets, data)
+    attr = data.split('_')[0]
 
-print(sum_iou / sum_frames)
+    num, num_suc, rate = tracking(path)
+    if num == -1:
+        break
+    sum_succ[attr] += num_suc
+    sum_frames[attr] += num
+    print(f'%s %.2f' % (data, rate * 100))
+
+  for attr, succ in sum_succ:
+     frames = sum_frames[attr]
+     rate = succ / frames * 100
+     print(f'%s %.2f' % (attributes[attr], rate))
+  succ_rate = sum_succ / sum_frames
+  print(f'Success rate on TinyTLP: %.2f' % (succ_rate * 100))
+
+def runTinyTLP():
+  dir_datasets = './TinyTLP'
+
+  all_data = os.listdir(dir_datasets)
+  sum_frames = 0
+  sum_succ = 0
+  for data in all_data:
+    path = os.path.join(dir_datasets, data)
+    num, num_suc, rate = tracking(path)
+    if num == -1:
+        break
+    sum_succ += num_suc
+    sum_frames += num
+    print(f'%s %.2f' % (data, rate * 100))
+
+  succ_rate = sum_succ / sum_frames
+  print(f'Success rate on TinyTLP: %.2f' % (succ_rate * 100))
+
+if __name__ == '__main__':
+  #runTinyTLP()
+  runTLPAtrr()
 """
   to create requirements.txt, run:
   py -m pipreqs.pipreqs .
